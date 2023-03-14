53e615668391565106b57afcf0148bb39e332e73
bilbo <bilbo.derbyshire@gmail.com>
2023-03-14 22:42:58 +0000

    Lesson 7 finish

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
     import 'modern-normalize/modern-normalize.css';
     import '../styles/main.scss';
-</script>
+	import type { LayoutData } from './$types';
 
+    export let data: LayoutData;
+</script>
+{data.user?.display_name}
 <slot />
