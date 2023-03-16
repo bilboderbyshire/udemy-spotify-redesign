5243e6e376754bdfee7e8f13801ffae0db81563e
bilbo <bilbo.derbyshire@gmail.com>
2023-03-16 18:01:41 +0000

    Tidied up icon buttons for sidebar

--- src/lib/components/Navigation.svelte
+++ src/lib/components/Navigation.svelte
@@ -1,237 +1,265 @@
 <script lang="ts">
-    import { tick, type ComponentType } from "svelte";
-    import { Home, Search, ListMusic, type Icon, Menu } from "lucide-svelte";
-    import logo from "$assets/spotify_logo_full_white.png";
-	import { page } from "$app/stores";
-	import { fade } from "svelte/transition";
-	import { beforeNavigate } from "$app/navigation";
-    import { IconButton } from "$components";
+	import { tick, type ComponentType } from 'svelte';
+	import { Home, Search, ListMusic, type Icon, Menu, X } from 'lucide-svelte';
+	import logo from '$assets/spotify_logo_full_white.png';
+	import { page } from '$app/stores';
+	import { fade } from 'svelte/transition';
+	import { beforeNavigate } from '$app/navigation';
+	import { IconButton } from '$components';
 
-    export let desktop: boolean = true;
+	export let desktop: boolean = true;
 
-    let isMobileMenuOpen = false;
-    $: isOpen = desktop || isMobileMenuOpen;
+	let isMobileMenuOpen = false;
+	$: isOpen = desktop || isMobileMenuOpen;
 
-    let openMenuButton: HTMLButtonElement;
-    let closeMenuButton: HTMLButtonElement;
-    let lastFocusableElement: HTMLAnchorElement;
+	let openMenuButton: IconButton;
+	let closeMenuButton: IconButton;
+	let lastFocusableElement: HTMLAnchorElement;
 
-    const menuItems: { path: string; label: string; icon: ComponentType<Icon> }[] = [
-        {
-            path: "/",
-            label: "Home",
-            icon: Home
-        },
-        {
-            path: "/search",
-            label: "Search",
-            icon: Search
-        },
-        {
-            path: "/playlists",
-            label: "Playlists",
-            icon: ListMusic
-        }
-    ];
-    
-    async function openMenu() {
-        isMobileMenuOpen = true;
-        await tick();
-        closeMenuButton.focus();
-    }
+	const menuItems: { path: string; label: string; icon: ComponentType<Icon> }[] = [
+		{
+			path: '/',
+			label: 'Home',
+			icon: Home
+		},
+		{
+			path: '/search',
+			label: 'Search',
+			icon: Search
+		},
+		{
+			path: '/playlists',
+			label: 'Playlists',
+			icon: ListMusic
+		}
+	];
 
-    function closeMenu() {
-        isMobileMenuOpen = false;
-        openMenuButton.focus();
-    }
+	async function openMenu() {
+		isMobileMenuOpen = true;
+		await tick();
+		closeMenuButton.getButton().focus();
+	}
 
-    function moveFocusToBottom(e: KeyboardEvent) {
-        if (desktop) return;
-        if (e.key === "Tab" && e.shiftKey) {
-            e.preventDefault()
-            lastFocusableElement.focus()
-        }
-    }
+	function closeMenu() {
+		isMobileMenuOpen = false;
+		openMenuButton.getButton().focus();
+	}
 
-    function moveFocusToTop(e: KeyboardEvent) {
-        if (desktop) return;
-        if (e.key == "Tab" && !e.shiftKey) {
-            e.preventDefault()
-            closeMenuButton.focus()
-        }
-        
-    }
+	function moveFocusToBottom(e: KeyboardEvent) {
+		if (desktop) return;
+		if (e.key === 'Tab' && e.shiftKey) {
+			e.preventDefault();
+			lastFocusableElement.focus();
+		}
+	}
 
-    function escapeSidebar(e: KeyboardEvent) {
-        if (desktop) return;
-        if (e.key === "Escape") {
-            closeMenu();
-        }
-    }
+	function moveFocusToTop(e: KeyboardEvent) {
+		if (desktop) return;
+		if (e.key == 'Tab' && !e.shiftKey) {
+			e.preventDefault();
+			closeMenuButton.getButton().focus();
+		}
+	}
 
-    beforeNavigate(() => {
-        isMobileMenuOpen = false;
-    })
+	function escapeSidebar(e: KeyboardEvent) {
+		if (desktop) return;
+		if (e.key === 'Escape') {
+			closeMenu();
+		}
+	}
+
+	beforeNavigate(() => {
+		isMobileMenuOpen = false;
+	});
 </script>
 
 <svelte:head>
-    {#if !desktop && isMobileMenuOpen}
-        <style>
-            body {
-                overflow: hidden;
-            }
-        </style>
-    {/if}
+	{#if !desktop && isMobileMenuOpen}
+		<style>
+			body {
+				overflow: hidden;
+			}
+		</style>
+	{/if}
 </svelte:head>
 
 <div class="nav-content" class:desktop class:mobile={!desktop}>
-    {#if !desktop && isMobileMenuOpen}
-        <div class="overlay" on:click={closeMenu} transition:fade={{duration: 200}} on:keydown={escapeSidebar}/>
-    {/if}
-    <nav aria-label="Main">
-        {#if !desktop}
-            <IconButton icon={Menu} label="Open menu"/>
-            <button bind:this={openMenuButton} on:click={openMenu} aria-expanded={isOpen}>Open</button>
-        {/if}
-        <div 
-            class="nav-content-inner" 
-            class:is-hidden={!isOpen} 
-            style:visibility={isOpen ? "visible" : "hidden"} 
-            on:keyup={escapeSidebar}
-        >
-            {#if !desktop}
-                <button bind:this={closeMenuButton} on:click={closeMenu} on:keydown={moveFocusToBottom}>Close</button>
-            {/if}
-            <img src={logo} class="logo" alt="spotify logo"/>
-            <ul>
-                {#each menuItems as item, index}
-                    {@const iconProps = {
-                        size: 25,
-                        focusable: 'false',
-                        'aria-hidden': true
-                    }}
-                    <li class:active={item.path === $page.url.pathname}>
-                        {#if menuItems.length === index + 1}
-                            <a bind:this={lastFocusableElement} href="{item.path}" on:keydown={moveFocusToTop}>
-                                <svelte:component this={item.icon} {...iconProps}/>
-                                <p>{item.label}</p>
-                            </a>
-                        {:else}
-                            <a href="{item.path}">
-                                <svelte:component this={item.icon} {...iconProps}/>
-                                <p>{item.label}</p>
-                            </a>
-                        {/if}
-                    </li>
-                {/each}
-            </ul>
-            {desktop}
-        </div>
-    </nav>
+	{#if !desktop && isMobileMenuOpen}
+		<div
+			class="overlay"
+			on:click={closeMenu}
+			transition:fade={{ duration: 200 }}
+			on:keydown={escapeSidebar}
+		/>
+	{/if}
+	<nav aria-label="Main">
+		{#if !desktop}
+			<IconButton
+				icon={Menu}
+				label="Open menu"
+				bind:this={openMenuButton}
+				on:click={openMenu}
+				aria-expanded={isOpen}
+				class="menu-button"
+			/>
+		{/if}
+		<div
+			class="nav-content-inner"
+			class:is-hidden={!isOpen}
+			style:visibility={isOpen ? 'visible' : 'hidden'}
+			on:keyup={escapeSidebar}
+		>
+			{#if !desktop}
+				<IconButton
+					icon={X}
+					iconColor="--faux-white"
+					label="Close menu"
+					bind:this={closeMenuButton}
+					on:click={closeMenu}
+					on:keydown={moveFocusToBottom}
+					class="close-menu-button"
+				/>
+			{/if}
+			<img src={logo} class="logo" alt="spotify logo" />
+			<ul>
+				{#each menuItems as item, index}
+					{@const iconProps = {
+						size: 25,
+						focusable: 'false',
+						'aria-hidden': true
+					}}
+					<li class:active={item.path === $page.url.pathname}>
+						{#if menuItems.length === index + 1}
+							<a bind:this={lastFocusableElement} href={item.path} on:keydown={moveFocusToTop}>
+								<svelte:component this={item.icon} {...iconProps} />
+								<p>{item.label}</p>
+							</a>
+						{:else}
+							<a href={item.path}>
+								<svelte:component this={item.icon} {...iconProps} />
+								<p>{item.label}</p>
+							</a>
+						{/if}
+					</li>
+				{/each}
+			</ul>
+			{desktop}
+		</div>
+	</nav>
 </div>
 
 <style lang="scss">
-    .nav-content {
+	.nav-content {
+		.overlay {
+			position: fixed;
+			width: 100%;
+			height: 100%;
+			top: 0;
+			left: 0;
+			background-color: var(--dark-gray);
+			opacity: 0.75;
+			z-index: 100;
 
-        .overlay {
-            position: fixed;
-            width: 100%;
-            height: 100%;
-            top: 0;
-            left: 0;
-            background-color: var(--dark-gray);
-            opacity: 0.75;
-            z-index: 100;
+			@include breakpoint.up('md') {
+				display: none;
+			}
+		}
 
-            @include breakpoint.up("md") {
-                display: none;
-            }
-        }
+		.logo {
+			max-width: 100%;
+			width: 150px;
+		}
 
-        .logo {
-            max-width: 100%;
-            width: 150px;
-        }
+		.nav-content-inner {
+			padding: 20px;
+			min-width: var(--sidebar-width);
+			background-color: var(--sidebar-color);
+			height: 100vh;
+			overflow: auto;
+			display: none;
 
-        .nav-content-inner {
-            padding: 20px;
-            min-width: var(--sidebar-width);
-            background-color: var(--sidebar-color);
-            height: 100vh;
-            overflow: auto;
-            display: none;
+			ul {
+				padding: 0;
+				margin: 20px 0 0;
+				list-style: none;
 
-            ul {
-                padding: 0;
-                margin: 20px 0 0;
-                list-style: none;
+				li {
+					&.active {
+						a {
+							opacity: 1;
+						}
+					}
 
-                li {
-                    &.active {
-                        a {
-                            opacity: 1;
-                        }
-                    }
-                    
-                    a {
-                        display: flex;
-                        max-height: fit-content;
-                        align-items: center;
-                        text-decoration: none;
-                        color: var(--light-text-color);
-                        font-size: functions.toRem(16);
-                        padding: 5px;
-                        margin: 10px 0;
-                        opacity: 0.7;
-                        transition: opacity 0.2s;
+					a {
+						display: flex;
+						max-height: fit-content;
+						align-items: center;
+						text-decoration: none;
+						color: var(--light-text-color);
+						font-size: functions.toRem(16);
+						padding: 5px;
+						margin: 10px 0;
+						opacity: 0.7;
+						transition: opacity 0.2s;
 
-                        &:hover, 
-                        &:focus {
-                            opacity: 1;
-                        }
-                    }
+						&:hover,
+						&:focus {
+							opacity: 1;
+						}
+					}
 
-                    :global(svg) {
-                        margin-right: 12px;
-                    }
+					:global(svg) {
+						margin-right: 12px;
+					}
 
-                    p {
-                        max-height: 100%;
-                        height: auto;
-                        margin: auto 0;
-                        font-weight: 500;
-                    }
-                }
-            };
-        }
+					p {
+						max-height: 100%;
+						height: auto;
+						margin: auto 0;
+						font-weight: 500;
+					}
+				}
+			}
+		}
 
-        &.desktop {
-            position: sticky;
-            top: 0;
-            .nav-content-inner {
-                @include breakpoint.up("md") {
-                    display: block;
-                }
-                
-            }
-        }
+		&.desktop {
+			position: sticky;
+			top: 0;
+			.nav-content-inner {
+				@include breakpoint.up('md') {
+					display: block;
+				}
+			}
+		}
 
-        &.mobile .nav-content-inner {
-            position: fixed;
-            top: 0;
-            left: 0;
-            z-index: 100;
-            transition: transform 200ms, opacity 200ms;
+		&.mobile .nav-content-inner {
+			position: fixed;
+			top: 0;
+			left: 0;
+			z-index: 100;
+			transition: transform 200ms, opacity 200ms;
 
-            &.is-hidden {
-                transition: transform 200ms, opacity 200ms, visibility 200ms;
-                transform: translateX(-100%);
-                opacity: 0;
-            }
+			&.is-hidden {
+				transition: transform 200ms, opacity 200ms, visibility 200ms;
+				transform: translateX(-100%);
+				opacity: 0;
+			}
 
-            @include breakpoint.down("md") {
-                display: block;
-            }
-        }
-    }
+			@include breakpoint.down('md') {
+				display: block;
+			}
+		}
+
+		:global(.menu-button) {
+			@include breakpoint.up('md') {
+				display: none;
+			}
+		}
+
+		:global(.close-menu-button) {
+			position: absolute;
+			right: 15px;
+			top: 15px;
+		}
+	}
 </style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,90 +1,93 @@
 <script lang="ts">
-	import { Navigation, Header } from "$components";
-    import 'modern-normalize/modern-normalize.css';
-    import '../styles/main.scss';
+	import { Navigation, Header } from '$components';
+	import 'modern-normalize/modern-normalize.css';
+	import '../styles/main.scss';
 	import type { LayoutData } from './$types';
 
-    let topbar: HTMLElement;
-    let scrollY: number;
-    let headerOpacity: number = 0;
+	let topbar: HTMLElement;
+	let scrollY: number;
+	let headerOpacity: number = 0;
 
-    // This data contains all the user information that was collected in the +layout.server.ts file. If there is a valid
-     // access token, this information will be from the Spotify API
-    export let data: LayoutData;
+	// This data contains all the user information that was collected in the +layout.server.ts file. If there is a valid
+	// access token, this information will be from the Spotify API
+	export let data: LayoutData;
 
-    // A simplification of the data, allowing us to specifically access the spotify user data
-    $: user = data.user;
-    $: if (topbar) {
-        headerOpacity = scrollY / topbar.offsetHeight < 1 ? scrollY / topbar.offsetHeight: 1;
-    }
+	// A simplification of the data, allowing us to specifically access the spotify user data
+	$: user = data.user;
+	$: if (topbar) {
+		headerOpacity = scrollY / topbar.offsetHeight < 1 ? scrollY / topbar.offsetHeight : 1;
+	}
 </script>
 
-<svelte:window bind:scrollY/>
+<svelte:window bind:scrollY />
 
 <div id="main">
-    {#if user}
-        <div id="sidebar">
-            <!-- When this component recieves true it will change the sidebar to appear all the time. When it is
+	{#if user}
+		<div id="sidebar">
+			<!-- When this component recieves true it will change the sidebar to appear all the time. When it is
                  false, the sidebar will appear when a button is pressed-->
-            <Navigation desktop={true}/>
-        </div>
-    {/if}
-    <div id="content">
-        {#if user}
-            <div id="topbar" bind:this={topbar}>
-                <div class="topbar-bg" style:background-color="var(--header-color)" style:opacity={headerOpacity}/>
-                <Header/>
-            </div>
-        {/if}
-        <main id="main-content" class:logged-in={user}>
-            <slot/>
-        </main>
-    </div>
+			<Navigation desktop={true} />
+		</div>
+	{/if}
+	<div id="content">
+		{#if user}
+			<div id="topbar" bind:this={topbar}>
+				<div
+					class="topbar-bg"
+					style:background-color="var(--header-color)"
+					style:opacity={`${headerOpacity}`}
+				/>
+				<Header />
+			</div>
+		{/if}
+		<main id="main-content" class:logged-in={user}>
+			<slot />
+		</main>
+	</div>
 </div>
 
 <style lang="scss">
-    #main {
-        display: flex;
+	#main {
+		display: flex;
 
-        #content {
-            flex: 1;
+		#content {
+			flex: 1;
 
-            #topbar {
-                position: fixed;
-                height: var(--header-height);
-                padding: 0 15px;
-                display: flex;
-                align-items: center;
-                width: 100%;
-                z-index: 100;
+			#topbar {
+				position: fixed;
+				height: var(--header-height);
+				padding: 0 15px;
+				display: flex;
+				align-items: center;
+				width: 100%;
+				z-index: 100;
 
-                .topbar-bg {
-                    position: absolute;
-                    width: 100%;
-                    height: 100%;
-                    top: 0;
-                    left: 0;
-                    z-index: -1;
-                    
-                }
+				.topbar-bg {
+					position: absolute;
+					width: 100%;
+					height: 100%;
+					top: 0;
+					left: 0;
+					z-index: -1;
+				}
 
-                @include breakpoint.up("md") {
-                    padding: 0 30px;
-                    width: calc(100% - var(--sidebar-width));
-                }
-            }
+				@include breakpoint.up('md') {
+					padding: 0 30px;
+					width: calc(100% - var(--sidebar-width));
+				}
+			}
 
-            main#main-content {
-                padding: 30px 15px 60px;
-            
-                @include breakpoint.up("md") {
-                    padding: 30px 30px 60px;
-                }
+			main#main-content {
+				padding: 30px 15px 60px;
 
-                &.logged-in {
-                    padding-top: calc(30px + var(--header-height));
-                }
-            }
-        }
-    }
-</style> 
+				@include breakpoint.up('md') {
+					padding: 30px 30px 60px;
+				}
+
+				&.logged-in {
+					padding-top: calc(30px + var(--header-height));
+				}
+			}
+		}
+	}
+</style>
