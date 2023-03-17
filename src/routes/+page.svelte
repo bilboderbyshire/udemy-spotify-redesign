5090d66b5b891fb1f5182a298e14f8b0eef80e39
bilbo <bilbo.derbyshire@gmail.com>
2023-03-17 22:27:21 +0000

    Collected all the data from the spotify api

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import type { PageData } from "./$types";
 
-<input />
 
-<select>
-    <option>Bye</option>
-</select>
-<button>Button</button>
-<div style="height: 1000px"/>
-
-<style lang="scss">
-    h1,
-    p {
-        color: var(--text-color);
-    }
-</style>
+    export let data: PageData;
+    $: console.log(data);
+</script>
